62adf353ca1e906181760ee7e764bb468f249560
christianoviedo <christian.oviedo@gmail.com>
2025-11-18 15:42:09 -0300

    Update chk_free_space.sh

--- chk_free_space.sh
+++ chk_free_space.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
-# set -x
-# Shell script to monitor or watch the disk space
-# It will send an email to $ADMIN, if the (free available) percentage of space is >= 90%.
-# -------------------------------------------------------------------------
-# Set admin email so that you can get email.
+# Shell script to monitor disk space
+# It will send an email to $ADMIN if the usage percentage is >= ALERT.
+
 ADMIN="my_email@email.com"
-# set alert level 90% is default
 ALERT=90
-# Exclude list of unwanted monitoring, if several partions then use "|" to separate the partitions.
-# An example: EXCLUDE_LIST="/dev/hdd1|/dev/hdc5"
 EXCLUDE_LIST="/auto/ripper|/snap"
-#
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#
+
 function main_prog() {
-while read output;
-do
-  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1)
-  partition=$(echo $output | awk '{print $2}')
-  if [ $usep -ge $ALERT ] ; then
-    echo "Running out of space \"$partition ($usep%)\" on server $(hostname), $(date)" | \
-      mail -s "Alert: Almost out of $1 $usep%" $ADMIN
-  fi
-done
+  while read usep partition; do
+    # Quitar el símbolo % si viene con él
+    usep=${usep%\%}
+
+    # Saltar líneas que no tengan número
+    if ! [[ "$usep" =~ ^[0-9]+$ ]]; then
+      continue
+    fi
+
+    if [ "$usep" -ge "$ALERT" ]; then
+      echo "Running out of space \"$partition ($usep%)\" on server $(hostname), $(date)" | \
+        mail -s "Alert: Almost out of disk space on $(hostname): $partition $usep%" "$ADMIN"
+    fi
+  done
 }
 
 _excluded="^Filesystem|tmpfs|cdrom"
-if [ "$EXCLUDE_LIST" != "" ] ; then
+if [ -n "$EXCLUDE_LIST" ]; then
   _excluded="${_excluded}|${EXCLUDE_LIST}"
 fi
 
-# disk usage
-df -H | grep -vE "${_excluded}" | awk '{print $5 " " $6}' | main_prog "disk space"
-# inode usage
-df -iH | grep -vE "${_excluded}" | awk '{print $5 " " $6}' | main_prog "inodes"
+# Obtener uso de disco y pasarlo a la función
+df -P -h | awk 'NR>1 {print $5 " " $6}' | egrep -v "$_excluded" | main_prog
